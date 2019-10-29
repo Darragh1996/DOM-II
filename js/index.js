e1abe9ed045efc1c23af42c61a5c38169288525d
Darragh Ferry <darraghferry@hotmail.com>
2019-10-29 13:47:37 +0000

    implemented a few event listeners, sorry I forgot to commit regularly

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
 // Your code goes here
+
+//mouseover
+let busPicture = document.querySelector('img');
+busPicture.addEventListener('mouseover', function(event){
+    busPicture.style.filter = 'grayscale(100%)';
+    busPicture.style.transition = '1s';
+});
+
+//mouseout
+busPicture.addEventListener('mouseout', function(event){
+    busPicture.style.filter = 'grayscale(0%)';
+    busPicture.style.transition = '1s';
+});
+
+//keydown
+let audio = new Audio('/sounds/Rudy_rooster_crowing-Shelley-1948282641.mp3');
+document.addEventListener('keydown', function(event){
+    audio.play();
+});
+
+//wheel
+let intro = document.querySelector('.intro');
+intro.addEventListener('wheel', function(event){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    intro.style.color = color;
+});
+
+//drag
+let text = document.querySelector('.text-content');
+text.addEventListener('drag', function(event){
+    text.textContent = 'PLEASE DONT DRAG ME';
+});
+
+//drop
+text.addEventListener('drop', function(event){
+    text.textContent = 'You ruined me';
+});
+
+//load
+// window.addEventListener('load', function(event){
+//     alert('Page has finished loading');
+// });
+
+//dblclick
+images = document.querySelectorAll('img');
+for(let i = 0; i < images.length; i++){
+    images[i].addEventListener('dblclick', function(event){
+        event.target.setAttribute('src', '/img/juan-encalada-6mcVaoGNz1w-unsplash.jpg');
+    });
+}
+
